b72b08ccae5f67e993fdf3bc29b5b4da3e56530c
KateSinelnikova <sinelnikova.13@mail.ru>
2020-05-28 20:00:21 +0300

    Синельникова -- Работа#6

---
+++ SinelnikovaK/Работа№6/Работа№6/Bulls_Cows.cpp
@@ -0,0 +1,62 @@
+#include "Bulls_Cows.h"
+using namespace std;
+
+Bulls_Cows::Bulls_Cows()
+{
+	length = 0;
+	number = nullptr;
+}
+
+Bulls_Cows::~Bulls_Cows()
+{
+	if (length > 0)
+		delete[] number;
+	length = 0;
+}
+
+bool Bulls_Cows::newNumber(const int length)
+{
+	if (length < 1 || length > 10)
+		return false;
+	number = new int[length];
+	this->length = length;
+	srand((int)time(0));
+	for (int i = 0; i < length; i++)
+	{
+		number[i] = rand() % 10;
+		for (int j = 0; j < i; j++)
+			if (number[i] == number[j])
+			{
+				i--;
+				break;
+			}
+	}
+	return true;
+}
+
+
+bool Bulls_Cows::checkCorrect(const string num)
+{
+	if (num.length() != length)
+		return false;
+	for (int i = 0; i < length - 1; i++)
+		for (int j = i + 1; j < length; j++)
+			if (num[i] == num[j])
+				return false;
+	return true;
+}
+
+bool Bulls_Cows::checkBullCow(string num, int& bull, int& cow)
+{
+	cow = bull = 0;
+	for (int i = 0; i < length; i++)
+		for (int j = 0; j < length; j++)
+			if (number[i] == num[j] - '0')
+				if (i == j)
+					bull++;
+				else
+					cow++;
+	if (bull == length)
+		return true;
+	return false;
+}
