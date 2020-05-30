c1f8affd382eee19b22f9b1cd1a519bbb0514eb5
ArAnGor <gorbart01@mail.ru>
2020-05-30 17:45:49 +0300

    Добавил Task_3_6

---
+++ GorbachevArtem/P_3_6/P_3_6/Dictionary.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Dictionary
+{
+private:
+	vector<string> en;
+	vector<string> ru;
+	int IndexOfWord(const vector<string>& vec, const string word) const;
+public:
+	Dictionary();
+	Dictionary(int n, const string _en[], const string _ru[]);
+	Dictionary(const vector<string>& _en, const vector<string>& _ru);
+	Dictionary(const Dictionary& D);
+	~Dictionary();
+	void AddWord(const string eWord, const string rWord);
+	void ChangeTranslation(const string word, const string translation);
+	string Translate(const string word) const;
+	bool IsContain(const string word) const;
+	int Size() const;
+	friend ofstream& operator<<(ofstream& out, const Dictionary& D);
+	friend ifstream& operator>>(ifstream& in, Dictionary& D);
+	Dictionary& operator=(const Dictionary& D);
+	Dictionary operator+(const Dictionary& D) const;
+	Dictionary& operator+=(const Dictionary& D);
+};
