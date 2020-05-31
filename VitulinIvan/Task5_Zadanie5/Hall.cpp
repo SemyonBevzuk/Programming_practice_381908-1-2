6bd3a687e5ad3232fd6e6f429255cc0fe852c26a
VitulinIvan <Ivan2008ovskiy@yandex.ru>
2020-05-31 16:39:27 +0300

    Task5_Zadanie5 Версия 1.0.0

---
+++ VitulinIvan/Task5_Zadanie5/Hall.cpp
@@ -0,0 +1,13 @@
+#include "Hall.h"
+
+Hall Halls::getHall(int num)
+{
+    switch (num)
+    {
+    case 1: return Hall{ 30, 10, 3, 10 };
+    case 2: return Hall{ 10, 10, 2, 10 };
+    case 3: return Hall{ 20, 20, 3, 20 };
+    default:
+        break;
+    }
+}
---
+++ VitulinIvan/Task5_Zadanie5/Session.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "DateTime.h"
+#include <vector>
+#include <map>
+#include "DateTime.h"
+#include "Hall.h"
+
+using namespace std;
+
+class Ticket;
+
+class Session
+{
+    string myFilmName;
+    DateTime myDate;
+    //true - free
+    bool** myTickets;
+    bool** myVIPTickets;
+    int myTicketsRowCount, myTicketsColumnCount, myVIPTicketsRowCount, myVIPTicketsColumnCount;
+    int myFreeTickets, myFreeVIPTickets;
+    int myHallNumber;
+public:
+    Session();
+    Session(string Name, int theHall, DateTime theDate);
+    Ticket CreateTicket(int Row, int Column, bool VIP);
+    int AllocatePlaces(int theTickets, int theVIPTikets, vector<Ticket>& theOutTickets);
+    void ReleasePlaces(const vector<Ticket>& theTickets);
+    void ReleasePlace(bool theVIP, int theRow, int theColumn);
+    DateTime GetData();
+    string ToString();
+    static int getPrice(DateTime time);
+};
+
+using namespace std;
+
+class Ticket
+{
+public:
+    string FilmName;
+    int Hall;
+    int Row, Column;
+    DateTime Date;
+    bool VIP;
+    Session mySession;
+    int price;
+    Ticket();
+    Ticket(string Name, int Hall, int Row, int Column, DateTime Date, bool VIP, int price) :
+        FilmName(Name), Hall(Hall), Row(Row), Column(Column), Date(Date), VIP(VIP), price(price) {}
+    Ticket(const Ticket& copy);
+    string ToString();
+};
