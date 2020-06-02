a1af154c06d26e56097423dde86b36963ac405ab
ArAnGor <gorbart01@mail.ru>
2020-06-02 14:56:22 +0300

    Добавил Task_6_1

---
+++ GorbachevArtem/P_6_1/P_6_1/Main.cpp
@@ -0,0 +1,31 @@
+﻿#include "Game.h"
+#include <iostream>
+
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int n;
+	cout << "Введите длину загадываемого числа: "; cin >> n;
+	while (n < 1 || n > 10)
+	{
+		cout << "Длина должна быть от 1 до 10!" << endl;
+		cin >> n;
+	}
+	Game game;
+	game.GenerateNumber(n);
+	string number;
+	while (game.GetBulls() != n)
+	{
+		cout << "--------------------------------------" << endl;
+		cout << "Попытайтесь отгадать число: "; cin >> number;
+		if (game.VerifyNumber(number))
+		{
+			cout << "Количество ""коров"": " << game.GetCows() << endl;
+			cout << "Количество ""быков"": " << game.GetBulls() << endl;
+		}
+		else
+			cout << "Введите число длины " << n << " с неповторяющимися цифрами!" << endl;
+	}
+	cout << "Вы отгадали всю последовательность!" << endl;
+	system("pause");
+}
